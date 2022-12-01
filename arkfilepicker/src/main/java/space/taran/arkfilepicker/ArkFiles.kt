87cb3846072eee47263b8c92409f628613bace0b
mdrlzy <mordeniusss@gmail.com>
2022-12-01 18:08:47 +0600

    Added .ark-global folder

--- arkfilepicker/src/main/java/space/taran/arkfilepicker/ArkFiles.kt
+++ arkfilepicker/src/main/java/space/taran/arkfilepicker/ArkFiles.kt
@@ -2,6 +2,7 @@ package space.taran.arkfilepicker
 
 import space.taran.arkfilepicker.ArkFiles.ARK_FOLDER
 import space.taran.arkfilepicker.ArkFiles.FAVORITES_FILE
+import space.taran.arkfilepicker.ArkFiles.GLOBAL_FOLDER
 import space.taran.arkfilepicker.ArkFiles.PREVIEWS_FOLDER
 import space.taran.arkfilepicker.ArkFiles.ROOTS_FILE
 import space.taran.arkfilepicker.ArkFiles.TAGS_STORAGE_FILE
@@ -10,6 +11,7 @@ import java.nio.file.Path
 
 internal object ArkFiles {
     const val ARK_FOLDER = ".ark"
+    const val GLOBAL_FOLDER = ".ark-global"
     const val ROOTS_FILE = "roots"
     const val FAVORITES_FILE = "favorites"
     const val TAGS_STORAGE_FILE = "tags"
@@ -19,6 +21,7 @@ internal object ArkFiles {
 
 internal fun Path.arkFolder() = resolve(ARK_FOLDER)
 internal fun Path.arkRoots() = resolve(ROOTS_FILE)
+internal fun Path.arkGlobal() = resolve(GLOBAL_FOLDER)
 internal fun Path.arkFavorites() = resolve(FAVORITES_FILE)
 internal fun Path.arkTagsStorage() = resolve(TAGS_STORAGE_FILE)
 internal fun Path.arkPreviews() = resolve(PREVIEWS_FOLDER)
