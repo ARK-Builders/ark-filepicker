9f1925d2adcef1abe4767e8f0b4ecaf96671b2e2
mdrlzy <mordeniusss@gmail.com>
2022-12-03 01:07:12 +0600

    rootsFirstPage option

--- arkfilepicker/src/main/java/space/taran/arkfilepicker/ArkFilePickerConfig.kt
+++ arkfilepicker/src/main/java/space/taran/arkfilepicker/ArkFilePickerConfig.kt
@@ -23,6 +23,7 @@ class ArkFilePickerConfig(
     val themeId: Int = com.google.android.material.R.style.Theme_MaterialComponents_Light_Dialog,
 
     val showRoots: Boolean = false,
+    val rootsFirstPage: Boolean = false,
     val initialPath: Path? = null,
     val mode: ArkFilePickerMode = ArkFilePickerMode.FOLDER,
     val pathPickedRequestKey: String? = null,
